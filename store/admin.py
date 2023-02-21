58fbf4f9ecc17c45031132b1263e1f28ffd55c04
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-21 13:15:57 +0200

    Error

--- store/admin.py
+++ store/admin.py
@@ -6,9 +6,9 @@ from . models import Product, Category
 @admin.register(Category) #we want create pre populated field
 class CategoryAdmin(admin.ModelAdmin):
 
-    prepopulated_fields = {'slug':{'name',}}
+    prepopulated_fields = {'slug':('name',)}
 
 @admin.register(Product) 
 class ProductAdmin(admin.ModelAdmin):
 
-    prepopulated_fields = {'slug':{'title',}}
+    prepopulated_fields = {'slug':('title',)}
