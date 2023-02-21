9b37d8a22791297f00fd5eb8eda58d49d991f0cb
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-21 13:49:25 +0200

    22 Configure our URL files and store view

--- eshop/urls.py
+++ eshop/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings #var media and..
 #it helps to make url path
 from django.conf.urls.static import static #connection between base root and static files especially medias
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')), #here we connect urls from store to project
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)      #MEDIA_URL this is why we imported settings we can connect to var we imported
