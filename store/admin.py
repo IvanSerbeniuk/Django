6a6c331e2b89f8cba93eb00cd9f107e9758d2632
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-21 13:13:09 +0200

    Декоратор для pre populated field

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from . models import Product, Category
 
-admin.site.register(Category)
 
-admin.site.register(Product)
+@admin.register(Category) #we want create pre populated field
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug':{'name',}}
+
+@admin.register(Product) 
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug':{'title',}}
