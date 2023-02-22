ad75020db1a7d6c9aa30c28f16d87ecb129c5c2c
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-22 12:56:37 +0200

    view page

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
     path('', views.store, name='store'),
 
-
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from . models import Category, Product
+from django.shortcuts import get_object_or_404
+
 
 def store(request):
  
@@ -15,3 +17,10 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return{'all_categories':all_categories}
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {'product':product}
+
+    return render(request, 'store/product-info.html', context)
