4040d3ae2215f5b1c0d398a7572c2856e6405cdf
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-22 16:16:01 +0200

    confic basic url and views for cart

--- eshop/urls.py
+++ eshop/urls.py
@@ -6,6 +6,12 @@ from django.conf import settings #var media and..
 from django.conf.urls.static import static #connection between base root and static files especially medias
 
 urlpatterns = [
+    #Admin url
     path('admin/', admin.site.urls),
+
+    #Store app
     path('', include('store.urls')), #here we connect urls from store to project
+
+    #Cart app
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)      #MEDIA_URL this is why we imported settings we can connect to var we imported
