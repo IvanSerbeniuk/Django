ad703c2d646eb57977b95fe4201c9ababf3fc49a
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-26 23:37:19 +0200

    60 row URL's and view

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
 
     path('register', views.register, name='register'), #account/register --> http resronse(views.py) - Account url setup!
 
+    #Email verification URL's
+    path('email_verification', views.email_verification, name='email-verification'),
 
+    path('email_verification-sent', views.email_verification_sent, name='email-verification-sent'),
 
+    path('email_verification-success', views.email_verification_success, name='email-verification-success'),
 
+    path('email_verification-failed', views.email_verification_failed, name='email-verification-failed'),
 
 ]
