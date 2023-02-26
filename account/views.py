f4749518670a9abeaf64ad98a51f8be470a776d4
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-26 19:44:58 +0200

    client register

--- account/views.py
+++ account/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm 
 
-from django.http import HttpResponse
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save() #its like sending final post request
+
+            return redirect('')
+
+    context = {'form':form}
 
 
-def register(requst):
 
-    return render(requst, 'account/registration/register.html')
+    return render(request, 'account/registration/register.html', context=context)
