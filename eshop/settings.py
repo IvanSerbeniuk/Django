83f20b734f4b9982eb8215c709547761dc0a6b99
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-02-28 15:30:47 +0200

    65 setup Email backend

--- eshop/settings.py
+++ eshop/settings.py
@@ -140,4 +140,14 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = '587' # default port for gmail
+
+EMAIL_USE_TLS = 'True'
+
+EMAIL_USER = 'serbeniuk.ivan@gmail.com' # My gmail address
+
+EMAIL_HOST_PASSWORD = 'scoubiqrwefyrhkd' # Enter your app password
