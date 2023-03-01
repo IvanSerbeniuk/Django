ab6ac09b217b9eb833063e7dc212c24ba4df201e
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-01 15:11:25 +0200

    72 Integreted profile templates

--- account/urls.py
+++ account/urls.py
@@ -22,8 +22,14 @@ urlpatterns = [
 
     path('user-logout', views.user_logout, name='user-logout'),
  
- 
+    # Dashboard / profile URLs
     path('dashboard', views.dashboard, name='dashboard'),
+
+    path('profile-management', views.profile_management, name='profile-management'),
+
+    path('delete-account', views.delete_account, name='delete-account'),
+
+    
 
 
 ]
