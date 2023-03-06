6b8d03f076910153975a9b1cea4333e2c6c56851
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-06 18:46:13 +0200

    91 Building the shipping model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ShippingAddress(models.Model):
+
+    full_name = models.CharField(max_length=150)
+
+    email = models.EmailField(max_length=255)
+
+    address1 = models.CharField(max_length=150)
+
+    address2 = models.CharField(max_length=150)
+
+    city = models.CharField(max_length=255)
+
+    
+    # Optional
+    state = models.CharField(max_length=255, null=True, blank=True)# Null for our database. Blank for user side 
+
+    zipcode = models.CharField(max_length=255, null=True, blank=True)
+
+    # Foreign Key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) #on_delete - when user deletes his account then shipping informaton also will be removedx
+
+    class Meta:
+
+        verbose_name_plural = 'Shipping Address'
+
+    
+    def __str__(self):
+
+        return 'Shipping Address - ' + str(self.id)
