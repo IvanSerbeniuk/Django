e96aee396f18ed9ebdcf02e435557bbec4c7430b
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-07 00:55:44 +0200

    96 i want to sleep

--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from .models import ShippingAddress
 
 def checkout(request):
 
-    return render(request, 'payment/checkout.html')
+    # Users with account -- Pre-fill the form
+    if request.user.is_authenticated: #if user login
+
+        try:
+            # Authinticated users WITH shippping info
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+
+            context = {'shipping': shipping_address}
+
+            return render(request, 'payment/checkout.html', context=context)
+
+        except:
+            # Authenticated user without shipping info
+            return render(request, 'payment/checkout.html')
+    
+    else: # Можно и без else :)
+        # Guest users
+        return render(request, 'payment/checkout.html')
 
 
 
