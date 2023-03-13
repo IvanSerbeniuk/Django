5f3c9c4ab924cbc117c687e0b67220c3ca5697d2
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-13 12:55:59 +0200

    100 Process order functionality (nt)

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
+
+from cart.cart import Cart
+
+from django.http import JsonResponse
+
 
 def checkout(request):
 
@@ -26,12 +31,60 @@ def checkout(request):
 
 def complete_order(request):
 
-    pass
+    if request.POST.get('action') == 'post':
 
+        name = request.POST.get('name')
+        email = request.POST.get('email')
 
+        address1 = request.POST.get('address1')
+        address2 = request.POST.get('address2')
+        city = request.POST.get('city')
 
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
 
+        # All-in-one shipping address
+        shipping_address = (address1 + '\n' + address2 + '\n' + city + '\n' + state + '\n' + zipcode)
 
+        # Shopping cart info
+        cart = Cart(request)
+
+        # Get total price of items
+        total_cost = cart.get_total()
+    
+    # 1) Creqte Order --> users with an account
+    if request.user.is_authenticated:
+
+        order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
+        amount_paid=total_cost, user=request.user)
+
+        order_id = order.pk
+
+        for item in cart:
+            
+            OrderItem.objects.create(order_id=order_id, product=item['item'], quantity=item['qty'],
+                                     price=item['price'], user=request.user)
+
+    # 2) Create order --> Guest user without an account
+    else:
+
+        if request.user.is_authenticated:
+
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
+            amount_paid=total_cost)                     #m Thank to blank=True, null=True in models we can not include  'user'
+
+            order_id = order.pk
+
+        for item in cart:
+            
+            OrderItem.objects.create(order_id=order_id, product=item['item'], quantity=item['qty'],
+                                     price=item['price'])
+
+    order_success = True 
+
+    response = JsonResponse({'success':order_success})
+
+    return response
 
 
 
